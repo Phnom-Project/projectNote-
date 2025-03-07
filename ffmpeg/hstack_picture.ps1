fcf8801ecee04d91f61c25a72a132db90a5134b1
vaysokra <51332878+vaysokra@users.noreply.github.com>
2025-03-07 22:26:07 +0700

    Create hstack_picture.ps1

---
+++ ffmpeg/hstack_picture.ps1
@@ -0,0 +1,41 @@
+# ../powershell.ps1
+
+# INPUT
+# - source files
+$inProcessPath = "D:\3ytest\yt-dlp\test_append\hstack\1";
+$isLandscape = $true;
+# VARIABLES
+$ffmpegPath =
+"D:\softwareInsteadDriveC\FFmpeg\ffmpeg-master-latest-win64-gpl\ffmpeg-master-latest-win64-gpl\bin\ffmpeg.exe";
+# - file name
+$fileName_output = "hstack.jpg"
+# - get childItem from folder
+$oldVideos =
+Get-ChildItem -Include @("*.png", "*.jpg", "*.jpeg") `
+    -Path $inProcessPath -Recurse;
+
+# output location
+Set-Location -Path $inProcessPath;
+
+# CONDITION
+# - check number of files
+if ($oldVideos.Count -gt 3) {
+    Write-Host -ForegroundColor Yellow "number object not allowed !" $oldVideos.Count
+    exit
+}
+Write-Host -ForegroundColor Green $oldVideos[0].Name
+# FFMPEG
+
+$var_crop =
+if ($isLandscape) { 'crop=iw/4:ih:(iw/2)-(iw/8):0,scale=-1:1280' }
+else { 'crop=iw/2:ih:iw/4:0,scale=-1:1080' };
+# - arguments
+$arg_hstack_picture = @'
+-i "{0}" -i "{1}" -i "{2}" -filter_complex
+"[0:v]{3}[left];
+[1:v]{3}[mid];
+[2:v]{3}[right];
+[left][mid][right]hstack=inputs=3" {4}
+'@ -f $oldVideos[0].Name, $oldVideos[1].Name, $oldVideos[2].Name, $var_crop, $fileName_output;
+# - hstack picture
+Start-Process -FilePath $ffmpegPath -ArgumentList $arg_hstack_picture -Wait -NoNewWindow;
