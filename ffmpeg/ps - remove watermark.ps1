4eddc85857d45c3dcad582edf511b8f4d41a6ccd
vaysokra <51332878+vaysokra@users.noreply.github.com>
2025-05-17 03:00:32 +0700

    Update ps - remove watermark.ps1

--- ffmpeg/ps - remove watermark.ps1
+++ ffmpeg/ps - remove watermark.ps1
@@ -1,10 +1,12 @@
 # ../powershell.ps1
+# watermark_remove
 
 
 # INPUT
 # - source files
-$inProcessPath = "D:\MMo\Sweetatoo\website\1\Ashley Carolina\0video\5\mixed"; # D:\M\4
-$inHaveWatermark = $false;
+$inProcessPath = "D:\MMo\website\1\0video\3\mixed"; # D:\M\4
+# 1:xfree, 2:picazor 21:picazor_landscape
+$inWatermarkType = 2;
 $inResolution = 720; # 1080, 720, 480
 # VARIABLES
 $ffmpegPath =
@@ -16,8 +18,10 @@ $oldVideos =
 Get-ChildItem -Include @("*.mp4") `
     -Path $inProcessPath -Recurse;
 
+
 # output location
 Set-Location -Path $inProcessPath;
+
 
 # CONDITION
 # - check number of files
@@ -28,23 +32,41 @@ if ($oldVideos.Count -gt 1) {
 Write-Host -ForegroundColor Green $oldVideos[0].Name
 # FFMPEG
 $var_resolution_multi =
-if($inResolution -eq 1080) {1080/1080}
-elseif ($inResolution -eq 720) {1080/720}
-elseif ($inResolution -eq 480) {1080/480}
+if ($inResolution -eq 1080) { 1080 / 1080 }
+elseif ($inResolution -eq 720) { 1080 / 720 }
+elseif ($inResolution -eq 480) { 1080 / 480 }
 else {
     Write-Host -ForegroundColor Yellow "number object not allowed !"
     exit
 }
 # - arguments
 $arg_removeWatermark =
-if($inHaveWatermark){
-@'
+if ($inWatermarkType -eq 1) {
+    # 1:xfree
+    @'
 -i {0} -vf "delogo=x=50/{2}:y=50/{2}:w=220/{2}:h=120/{2}:show=0,
 delogo=x=800/{2}:y=1750/{2}:w=220/{2}:h=120/{2}:show=0" -c:a copy {1}
 '@ -f $oldVideos[0].Name, $fileName_output, $var_resolution_multi;
 }
-else{
-@'
+elseif ($inWatermarkType -eq 2) {
+    # 2:picazor
+    @'
+-i {0} -vf
+"delogo=x=800/{2}:y=14/{2}:w=275/{2}:h=80/{2}:show=0" -c:a copy {1}
+'@ -f $oldVideos[0].Name, $fileName_output, $var_resolution_multi;
+}
+elseif ($inWatermarkType -eq 21) {
+    # 21:picazor_landscape
+    @'
+-i {0} -vf
+"delogo=x=14/{2}:y=14/{2}:w=400/{2}:h=100/{2}:show=0,
+delogo=x=1520/{2}:y=14/{2}:w=400/{2}:h=100/{2}:show=0,
+delogo=x=1520/{2}:y=970/{2}:w=400/{2}:h=100/{2}:show=0" -c:a copy {1}
+'@ -f $oldVideos[0].Name, $fileName_output, $var_resolution_multi;
+}
+else {
+    # no-watermark
+    @'
 -i {0} -vf "delogo=x=50:y=50:w=0:h=0:show=0" -c:a copy {1}
 '@ -f $oldVideos[0].Name, $fileName_output;
 }
