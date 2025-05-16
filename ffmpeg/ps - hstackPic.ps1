9e0cc89a65c462455ad682ce49bf600eb9651196
vaysokra <51332878+vaysokra@users.noreply.github.com>
2025-05-17 03:02:11 +0700

    Update ps - hstackPic.ps1

--- ffmpeg/ps - hstackPic.ps1
+++ ffmpeg/ps - hstackPic.ps1
@@ -1,10 +1,12 @@
 # ../powershell.ps1
+# hstack_pic
 
 
 # INPUT
 # - source files
-$inProcessPath = "D:\MMo\Sweetatoo\website\1\Anna Malygon\0video\1\append\thumb\hstack"; # D:\M\4
+$inProcessPath = "D:\MMo\website\1\0video\3\mixed\rw\thumb\select"; # D:\M\4
 $isLandscape = $false;
+$iPosition = 2; # 1left 2middle 3right
 # VARIABLES
 $ffmpegPath =
 "D:\softwareInsteadDriveC\FFmpeg\ffmpeg-master-latest-win64-gpl\ffmpeg-master-latest-win64-gpl\bin\ffmpeg.exe";
@@ -15,8 +17,10 @@ $oldVideos =
 Get-ChildItem -Include @("*.png", "*.jpg", "*.jpeg") `
     -Path $inProcessPath -Recurse;
 
+
 # output location
 Set-Location -Path $inProcessPath;
+
 
 # CONDITION
 # - check number of files
@@ -26,8 +30,14 @@ if ($oldVideos.Count -gt 3) {
 }
 Write-Host -ForegroundColor Green $oldVideos[0].Name
 # FFMPEG
+$var_position =
+if($iPosition -eq 1){'0'}
+elseif($iPosition -eq 3){'(iw/2)+(iw/8)'}
+else{'(iw/2)-(iw/8)'}
+
+
 $var_crop =
-if ($isLandscape) { 'crop=iw/4:ih:(iw/2)-(iw/8):0,scale=-1:1280' }
+if ($isLandscape) { 'crop=iw/4:ih:"{0}":0,scale=-1:1280' -f $var_position }
 else { 'crop=(iw*3)/4:(ih*5)/6:(iw)/4:(ih)/10,scale=-1:1280' };
 # - arguments
 $arg_hstack_picture = @'
@@ -37,5 +47,7 @@ $arg_hstack_picture = @'
 [2:v]{3}[right];
 [left][mid][right]hstack=inputs=3" {4}
 '@ -f $oldVideos[0].Name, $oldVideos[1].Name, $oldVideos[2].Name, $var_crop, $fileName_output;
+
 # - hstack picture
 Start-Process -FilePath $ffmpegPath -ArgumentList $arg_hstack_picture -Wait -NoNewWindow;
+
